24c78d74a4dcde4902f1846d981f3866df75ceb1
Henrique Pereira <henrique802011@gmail.com>
2021-06-06 16:23:06 -0300

    primeiro commit

---
+++ Js/app.js
@@ -0,0 +1,8 @@
+const passWordEl = document.getElementById("pw");
+const copyEl = document.getElementById("copy");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("number");
+const symbollEl = document.getElementById("symbols");
+const generateEl = document.getElementById("generate");
+
